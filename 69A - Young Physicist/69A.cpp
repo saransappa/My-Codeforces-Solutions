f6b8dfbdcca23d37261e1f550d46c4de517f4e9b
saransappa <saran.sappa@gmail.com>
2021-02-23 16:08:15 +0530

    added 69A

---
+++ 69A - Young Physicist/69A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl; // Debugging function
+#define ll long long 
+#define pb push_back
+#define elif else if
+
+template <typename... T>
+void read(T&... args){          // Function to read multiple arguments
+    ((cin>>args), ...);
+}
+
+template <typename... T>
+void write(T&... args){          // Function to write multiple arguments
+    ((cout<<args<<" "), ...);
+}
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+
+/*Code starts from here */
+
+void solve(){
+    int n;
+    cin>>n;
+    int x=0,y=0,z=0;
+    int a,b,c;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        x+=a;
+        y+=b;
+        z+=c;
+    }
+    if(x==0 && y==0 && z==0)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    solve();
+	return 0;
+}
