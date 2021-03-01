041bd137f608d7407cd75f6062e5b8df84c63bee
saransappa <saran.sappa@gmail.com>
2021-03-01 12:41:33 +0530

    added 1492A

---
+++ 1492A - Three Swimmers/1492A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl; // Debugging function
+#define ll long long 
+#define pb push_back
+#define elif else if
+
+template <typename... T>
+void read(T&... args){          // Function to read multiple arguments
+    ((cin>>args), ...);
+}
+
+template <typename... T>
+void write(T&... args){          // Function to write multiple arguments
+    ((cout<<args<<" "), ...);
+}
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+
+/*Code starts from here */
+
+void solve(){
+    long long int p,a,b,c;
+    cin>>p>>a>>b>>c;
+    long long int k = p%a;
+    long long int l = p%b;
+    long long int m = p%c;
+    if(k==0 || l==0 || m==0)cout<<"0"<<endl;
+    else cout<<min(a-k,min(b-l,c-m))<<endl;;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int t;   
+    cin>>t;
+    for(int y=0;y<t;y++){
+        solve();
+    }
+	return 0;
+}
