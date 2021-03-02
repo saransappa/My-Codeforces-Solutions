916084c2b39a7055d947805ea384c2afcce10bf5
saransappa <saran.sappa@gmail.com>
2021-03-02 13:14:38 +0530

    added 1480A

---
+++ 1480A - Yet Another String Game/1480A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl; // Debugging function
+#define ll long long 
+#define pb push_back
+#define elif else if
+
+template <typename... T>
+void read(T&... args){          // Function to read multiple arguments
+    ((cin>>args), ...);
+}
+
+template <typename... T>
+void write(T&... args){          // Function to write multiple arguments
+    ((cout<<args<<" "), ...);
+}
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+
+/*Code starts from here */
+
+void solve(){
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(i%2==0){
+            if(s[i]=='a')s[i]='b';
+            else s[i]='a';
+        }
+        else{
+            if(s[i]=='z')s[i]='y';
+            else s[i]='z';
+        }
+    }
+    cout<<s<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int t;   
+    cin>>t;
+    for(int y=0;y<t;y++){
+        solve();
+    }
+	return 0;
+}
